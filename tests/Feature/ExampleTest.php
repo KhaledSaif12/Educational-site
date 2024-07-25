b1d9b2e804c2a15ffcda11156e7527beead463a1
KhaledSaif12 <129229836+KhaledSaif12@users.noreply.github.com>
2024-07-25 12:40:59 -0700

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
@@ -12,8 +13,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
 
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/');
         $response->assertStatus(200);
     }
 }
